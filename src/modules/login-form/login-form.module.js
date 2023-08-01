f47c159e6e1e2bd8e8790a5970677b4a7b1ad104
Indra Tanaya <imadearyaindra.tanaya@csgi.com>
2023-08-01 20:29:52 +0800

    Create mock login functionality

--- src/modules/login-form/login-form.module.js
+++ src/modules/login-form/login-form.module.js
@@ -2,4 +2,19 @@
 
 angular.module("loginForm", []).component("loginForm", {
   templateUrl: "./modules/login-form/login-form.template.html",
+  controller: [
+    "$scope",
+    "$timeout",
+    function ($scope, $timeout) {
+      $scope.submitting = false;
+      $scope.creds = {};
+
+      $scope.login = function () {
+        $scope.submitting = true;
+        $timeout(function () {
+          $scope.submitting = false;
+        }, 1000);
+      };
+    },
+  ],
 });
