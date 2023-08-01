8e8dd8e343f3a9548f45495a0b54eb9bcad9f365
Indra Tanaya <imadearyaindra.tanaya@csgi.com>
2023-08-01 20:39:42 +0800

    Separate the component with the module

--- src/modules/login-form/login-form.module.js
+++ src/modules/login-form/login-form.module.js
@@ -1,22 +1,3 @@
 "use strict";
 
-angular.module("loginForm", []).component("loginForm", {
-  templateUrl: "./modules/login-form/login-form.template.html",
-  controller: [
-    "$timeout",
-    function ($timeout) {
-      this.submitting = false;
-      this.creds = {};
-
-      this.login = function () {
-        this.submitting = true;
-        $timeout(
-          function () {
-            this.submitting = false;
-          }.bind(this),
-          1000
-        );
-      };
-    },
-  ],
-});
+angular.module("loginForm", []);
