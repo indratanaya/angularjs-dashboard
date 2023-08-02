94547eb458d2c8c81513a7c8ff30d5b516acd05d
Indra Tanaya <imadearyaindra.tanaya@csgi.com>
2023-08-02 11:53:27 +0800

    Add redirection to home dashboard

--- src/app.config.js
+++ src/app.config.js
@@ -6,6 +6,9 @@ angular.module("app").config([
         templateUrl: "./pages/login.page.html",
       })
       .when("/dashboard", {
+        redirectTo: "/dashboard/home",
+      })
+      .when("/dashboard/home", {
         templateUrl: "./pages/dashboard.page.html",
       })
       .when("/404", {
