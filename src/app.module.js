16b458814ddedb92211afd7e7395615717b1ebfd
Indra Tanaya <imadearyaindra.tanaya@csgi.com>
2023-08-02 10:11:38 +0800

    Add navbar

--- src/app.module.js
+++ src/app.module.js
@@ -1,3 +1,3 @@
 "use strict";
 
-angular.module("app", ["ngRoute", "loginForm"]);
+angular.module("app", ["ngRoute", "loginForm", "navbar"]);
---
+++ src/modules/layout/navbar/navbar.component.js
@@ -0,0 +1,6 @@
+"use strict";
+
+angular.module("navbar").component("navbar", {
+  templateUrl: "./modules/layout/navbar/navbar.template.html",
+  controller: [function () {}],
+});
